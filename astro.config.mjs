13fd0347e804b0f9eefa83818c2122e59c907410
Max van Schoonderwoerd <max@elephantcs.nl>
2025-05-26 16:32:41 +0200

    update site URL and sitemap; enhance metadata and remove services page

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://maxvanschoonderwoerd.netlify.app/",
+  site: "https://maxvanschoonderwoerd.nl/",
   integrations: [
     mdx(),
     sitemap(),
--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Portfolio Max van Schoonderwoerd";
+export const SITE_TITLE = "Max van Schoonderwoerd | Portfolio";
 export const SITE_DESCRIPTION = "Persoonlijk portfolio van Max van Schoonderwoerd, webdeveloper, designer, programmer en student aan de Hogeschool Rotterdam";
